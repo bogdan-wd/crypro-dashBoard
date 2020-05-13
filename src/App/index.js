90510c902c6f4957ed39766504452c3bcbdb4f41
bogdan-wd <bogdan.miroshnik.dnepr@gmail.com>
2020-05-13 22:45:45 +0300

    base structure

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+import Applayout from './AppLayout';
+
+function App () {
+  return (
+    <Applayout>
+      <div className="App">
+        hello world
+      </div>
+    </Applayout>
+  );
+}
+
+export default App;
